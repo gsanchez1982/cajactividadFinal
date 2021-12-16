6b1140ce5b3b51f15819a4aa726421f60d79f686
Guillermo <gsemx@outlook.com>
2021-12-16 00:14:48 -0600

    Update IMC.java

--- src/main/java/net/codejava/entity/IMC.java
+++ src/main/java/net/codejava/entity/IMC.java
@@ -9,6 +9,7 @@ import java.time.format.DateTimeFormatter;
 
 @Entity
 public class IMC {
+<<<<<<< HEAD
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -83,5 +84,80 @@ public class IMC {
         String FechaFormat = miFecha.format(miFechaF);
         this.fecha = FechaFormat;
     }
+=======
+	private Long id;
+	private String name;
+	private float peso;
+	private float altura;
+        private float iMC;
+	private String fecha;
+
+	public IMC() {
+	}
+
+	protected IMC(Long id, String name, float peso, float altura, float iMC, String fecha) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.peso = peso;
+		this.altura = altura;
+		this.iMC = iMC;
+                this.fecha = fecha;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		
+                this.name = name;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	public float getAltura() {
+		return altura;
+	}
+
+	public void setAltura(float altura) {
+		this.altura = altura;
+	}
+
+	public float getIMC() {
+		return iMC;
+	}
+
+	public void setIMC(float altura, float peso) {
+                this.iMC = peso / (altura * altura);
+	}
+        
+        public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+                LocalDate miFecha = LocalDate.now();
+                DateTimeFormatter miFechaF = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+                String FechaFormat = miFecha.format(miFechaF);
+		this.fecha = FechaFormat;
+	}
+>>>>>>> parent of 478cf05 (Update IMC.java)
 
 }
